d246bd57b1994fca4091840b19aeb0fab96ddca0
LeanVel <velasco.l.n@gmail.com>
2017-06-23 15:27:10 +0200

    Add changes to compile in Linux

--- insert_dylib/main.c
+++ insert_dylib/main.c
@@ -7,9 +7,15 @@
 #include <getopt.h>
 #include <sys/stat.h>
 #include <sys/param.h>
-#include <copyfile.h>
+
 #include <mach-o/loader.h>
 #include <mach-o/fat.h>
+
+//Added by us
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
 
 #define IS_64_BIT(x) ((x) == MH_MAGIC_64 || (x) == MH_CIGAM_64)
 #define IS_LITTLE_ENDIAN(x) ((x) == FAT_CIGAM || (x) == MH_CIGAM_64 || (x) == MH_CIGAM)
@@ -21,6 +27,65 @@
 #define ABSDIFF(x, y) ((x) > (y)? (uintmax_t)(x) - (uintmax_t)(y): (uintmax_t)(y) - (uintmax_t)(x))
 
 #define BUFSIZE 512
+
+int cp(const char *to, const char *from)
+{
+    int fd_to, fd_from;
+    char buf[4096];
+    ssize_t nread;
+    int saved_errno;
+
+    fd_from = open(from, O_RDONLY);
+    if (fd_from < 0)
+        return -1;
+
+    fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, 0666);
+    if (fd_to < 0)
+        goto out_error;
+
+    while (nread = read(fd_from, buf, sizeof buf), nread > 0)
+    {
+        char *out_ptr = buf;
+        ssize_t nwritten;
+
+        do {
+            nwritten = write(fd_to, out_ptr, nread);
+
+            if (nwritten >= 0)
+            {
+                nread -= nwritten;
+                out_ptr += nwritten;
+            }
+            else if (errno != EINTR)
+            {
+                goto out_error;
+            }
+        } while (nread > 0);
+    }
+
+    if (nread == 0)
+    {
+        if (close(fd_to) < 0)
+        {
+            fd_to = -1;
+            goto out_error;
+        }
+        close(fd_from);
+
+        /* Success! */
+        return 0;
+    }
+
+  out_error:
+    saved_errno = errno;
+
+    close(fd_from);
+    if (fd_to >= 0)
+        close(fd_to);
+
+    errno = saved_errno;
+    return -1;
+}
 
 void fbzero(FILE *f, off_t offset, size_t len) {
 	static unsigned char zeros[BUFSIZE] = {0};
@@ -431,8 +496,8 @@ int main(int argc, const char *argv[]) {
 				exit(1);
 			}
 		}
-
-		if(copyfile(binary_path, new_binary_path, NULL, COPYFILE_DATA | COPYFILE_UNLINK)) {
+		
+		if(cp(new_binary_path, binary_path)){
 			printf("Failed to create %s\n", new_binary_path);
 			exit(1);
 		}
